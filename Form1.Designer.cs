937e46d2493619399d70cc531ae09bac4f754814
urochka21 <popmen67@gmil.com>
2021-10-21 15:30:22 +0400

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Laboratornaya_rabota_18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.songsDataGridView = new System.Windows.Forms.DataGridView();
+            this.addNewRowButton = new System.Windows.Forms.Button();
+            this.deleteRowButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.songsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // songsDataGridView
+            // 
+            this.songsDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.songsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.songsDataGridView.Location = new System.Drawing.Point(32, 12);
+            this.songsDataGridView.Name = "songsDataGridView";
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.songsDataGridView.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.songsDataGridView.Size = new System.Drawing.Size(338, 215);
+            this.songsDataGridView.TabIndex = 0;
+            this.songsDataGridView.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.songsDataGridView_CellFormatting);
+            // 
+            // addNewRowButton
+            // 
+            this.addNewRowButton.Location = new System.Drawing.Point(54, 248);
+            this.addNewRowButton.Name = "addNewRowButton";
+            this.addNewRowButton.Size = new System.Drawing.Size(75, 23);
+            this.addNewRowButton.TabIndex = 1;
+            this.addNewRowButton.Text = "Добавить строку";
+            this.addNewRowButton.UseVisualStyleBackColor = true;
+            this.addNewRowButton.Click += new System.EventHandler(this.addNewRowButton_Click);
+            // 
+            // deleteRowButton
+            // 
+            this.deleteRowButton.Location = new System.Drawing.Point(144, 248);
+            this.deleteRowButton.Name = "deleteRowButton";
+            this.deleteRowButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteRowButton.TabIndex = 2;
+            this.deleteRowButton.Text = "Удалить строку";
+            this.deleteRowButton.UseVisualStyleBackColor = true;
+            this.deleteRowButton.Click += new System.EventHandler(this.deleteRowButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deleteRowButton);
+            this.Controls.Add(this.addNewRowButton);
+            this.Controls.Add(this.songsDataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.songsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView songsDataGridView;
+        private System.Windows.Forms.Button addNewRowButton;
+        private System.Windows.Forms.Button deleteRowButton;
+    }
+}
+
